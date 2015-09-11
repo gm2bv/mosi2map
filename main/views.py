538143bf75468f9d17ccc747f397b70b63ece253
gm2bv <gm2bv2001@gmail.com>
2015-09-11 13:16:38 +0900

    [add] templates for 'index' and 'confirm'

--- main/views.py
+++ main/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
-##from django.template import RequestContext, loader
 
 def index(request):
-    context = { 'hoge': 'HOGEHOGE' }
+    context = { }
     return render(request, 'main/index.html', context);
 
-#    template = loader.get_template('main/index.html')
-#    context = RequestContext(request, {
-#        'hoge' : 'HOGEHOGE'
-#    })
-#    return HttpResponse(template.render(context))
-
 def confirm(request):
-    return HttpResponse("Confirm")
+    context = { }
+    return render(request, 'main/confirm.html', context);
 
 def regist(request):
     return HttpResponse("Regist")
